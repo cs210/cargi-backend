5c041d7991f6ead25bb36c635dbb930db265f090
Emily Tang <emjtang@stanford.edu>
2016-02-25 18:31:59 -0800

    skeleton for web app using web.py

---
+++ cargi-web-app/cargi.py
@@ -0,0 +1,38 @@
+import web, os
+render = web.template.render('templates/')
+
+urls = (
+    '/', 'index', '/confirmNavigation', 'confirmNavigation', '/confirmupdates', 'updates', '/navigation', 'navigation'
+)
+
+class index:
+    def GET(self):
+        return render.index();
+class confirmNavigation:
+    def GET(self):
+        return render.confirmNavigation();
+class updates:
+    def GET(self):
+        return render.confirmupdates();
+class navigation:
+   def GET(self):
+        return render.navigation();
+# class images:
+#     def GET(self,name):
+#         ext = name.split(".")[-1] # Gather extension
+
+#         cType = {
+#             "png":"images/png",
+#             "jpg":"images/jpeg",
+#             "gif":"images/gif",
+#             "ico":"images/x-icon"            }
+
+#         if name in os.listdir('images'):  # Security
+#             web.header("Content-Type", cType[ext]) # Set the Header
+#             return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
+#         else:
+#             raise web.notfound()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
---
+++ cargi-web-app/static/controller.js
@@ -0,0 +1,4 @@
+document.getElementById("next-btn").onclick = function() {
+  console.log("hello?");
+  location.href = "/templates/confirm-navigation.html";
+}
