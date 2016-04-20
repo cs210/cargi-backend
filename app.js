b1223393ca1d6d1bc6d4027ee935a6468530cdb1
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-04-20 15:45:52 -0700

    added tables for testing

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var app = express(),
 
 // Define a TodoItem table
 mobile.tables.add('TodoItem');
+mobile.tables.add('Users');
+mobile.tables.add('Location_history');
+
 
 // Add the mobile API so it is accessible as a Web API
 app.use(mobile);
