5c15ca836b3cd18ff0679550310b4af3c83fcdf2
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-04-23 16:52:21 -0700

    LocationHistory and ContactHistory tables

--- tables/Location_history.js
+++ tables/Location_history.js
@@ -14,9 +14,16 @@ table.dynamicSchema = false;
 
 // Seed data into the table
 table.seed = [
-    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-01-23T18:55:00+00:00"},
-    { user_id: "kartiks2", longitude: -122.165309, latitude: 37.425575, datetime: "2016-01-30T19:00:00+00:00"},
-    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-01-18T19:00:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-02T11:00:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.165309, latitude: 37.425575, datetime: "2016-04-09T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-16T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-11T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-12T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-13T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-14T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-15T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-16T11:30:00+00:00"},
+    { user_id: "kartiks2", longitude: -122.164089, latitude: 37.425421, datetime: "2016-04-17T11:30:00+00:00"},
     { user_id: "emjtang", longitude: -122.164089, latitude: 37.425421, datetime: "2016-01-18T19:00:00+00:00"}
 ];
 
