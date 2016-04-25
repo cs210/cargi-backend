a300658f419745100ae87a12a75888cf7627c545
Maya Balakrishnan <mayanb@gmail.com>
2016-04-25 00:29:58 -0700

    makes selenium request

--- seleniumtest.js
+++ seleniumtest.js
@@ -1,21 +1,3 @@
-
-// var webdriverio = require('webdriverio');
-// var options = { desiredCapabilities: { browserName: 'chrome' } };
-// var client = webdriverio.remote(options);
-
-// client
-//     .init()
-//     .url('https://duckduckgo.com/')
-//     .setValue('#search_form_input_homepage', 'WebdriverIO')
-//     .click('#search_button_homepage')
-//     .getTitle().then(function(title) {
-//         console.log('Title is: ' + title);
-
-//         // outputs:
-//         // "Title is: WebdriverIO (Software) at DuckDuckGo"
-//     })
-//     .end();
-
 var webdriverio = require('webdriverio');
 
 var options = {
@@ -28,25 +10,13 @@ var client = webdriverio.remote(options);
 
 client
     .init()
-    .url('https://news.ycombinator.com/')
-    .selectorExecute('//div', function(inputs, message){
-        return inputs.length + ' ' + message;
-    }, 'divs on the page')
-    .then(function(res){
-        console.log(res);
+    .url('https://gasbuddy.com/')
+    .timeoutsImplicitWait(5000).then(.executeScript("return window.stop();"))
+    .setValue('*[id="search-text"]','98052')
+    .click('*[id="search-btn"]')
+    .pause(1000)
+    .getTitle().then(function(title) {
+        console.log(title);
     })
     .end();
-
-// var webdriver = require('selenium-webdriver'),
-//     By = require('selenium-webdriver').By,
-//     until = require('selenium-webdriver').until;
-
-// var driver = new webdriver.Builder()
-//     .forBrowser('firefox')
-//     .build();
-
-// driver.get('http://www.google.com');
-// driver.findElement(By.name('q')).sendKeys('webdriver');
-// driver.findElement(By.name('btnG')).click();
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-// driver.quit();
+   
