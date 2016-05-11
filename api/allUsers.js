f1bc54b9733d39027f8dd589532768b30f041c71
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-05-10 18:31:18 -0700

    changed allUsers

--- api/allUsers.js
+++ api/allUsers.js
@@ -6,7 +6,7 @@
 
 var api = {
     get: function(request, response, next) {
-        var query = {sql: 'SELECT DISTINCT name,email from users'
+        var query = {sql: 'SELECT DISTINCT u.name as name, u.email as email, a.action as action, a.createdAt as actionTime, l.start_datetime as startTime, l.end_datetime as endTime, l.duration as duration from users u, actions_taken a, log l WHERE u.id = a.user_id AND u.id = l.user_id'
         };
         request.azureMobile.data.execute(query)
         .then(function(results) {
