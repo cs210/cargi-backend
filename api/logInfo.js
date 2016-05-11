d86256ecd07f53e50f4553153c6f4ca4a51f883d
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-05-10 18:25:41 -0700

    dummy logInfo

--- api/logInfo.js
+++ api/logInfo.js
@@ -6,7 +6,7 @@
 
 var api = {
     get: function(request, response, next) {
-        var query = {sql: 'SELECT DISTINCT u.name as name, u.email as email, a.action as action, a.createdAt as actionTime, l.start_datetime as startTime, l.end_datetime as endTime, l.duration as duration from users u, log l, actions_taken a where u.id = a.user_id and u.id = l.user_id'
+        var query = {sql: 'SELECT DISTINCT u.name as name, u.email as email from users u'
         };
         request.azureMobile.data.execute(query)
         .then(function(results) {
