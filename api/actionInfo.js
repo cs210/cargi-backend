8a89556c62452296dd7232f6141fb20c4eec4043
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-05-15 23:58:55 -0700

    separated log and action info into two APIs

--- api/actionInfo.js
+++ api/actionInfo.js
@@ -1,12 +1,12 @@
 /*
-*The logInfo API
+*The actionInfo API
 * Created by Kartik Sawhney on 5/8/2016
 * Copyright © 2016 Cargi. All rights reserved.
 */
 
 var api = {
     get: function(request, response, next) {
-        var query = {sql: 'SELECT u.name as name, u.email as email, l.start_datetime as startTime, l.end_datetime as endTime, l.duration as duration from users u, log l where u.id=l.user_id'
+        var query = {sql: 'SELECT u.name as name, u.email as email, a.action as action from users u, actions_taken a where u.id=a.user_id'
         };
         request.azureMobile.data.execute(query)
         .then(function(results) {
