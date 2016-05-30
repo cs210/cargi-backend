c4959af4264832f2da9d357dcaf24756f2726845
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-05-30 00:22:22 -0700

    manually implemented group by

--- api/filterContacts.js
+++ api/filterContacts.js
@@ -13,6 +13,48 @@ function checkForDuplicates(results, name) {
     return false;
 }
 
+function sortByFrequency(array) {
+    var frequency = {}
+    array.forEach(function(value) { frequency[value] = 0; });
+    var uniques = array.filter(function(value) {
+        return ++frequency[value] == 1;
+    });
+    return uniques.sort(function(a, b) {
+        return frequency[b] - frequency[a];
+    });
+}
+
+function sortByFrequencyAndRemoveDuplicates(array) {
+    var frequency = {}, value;
+
+    // compute frequencies of each value
+    for(var i = 0; i < array.length; i++) {
+        value = array[i];
+        if(value in frequency) {
+            frequency[value]++;
+        }
+        else {
+            frequency[value] = 1;
+        }
+    }
+
+    // make array from the frequency object to de-duplicate
+    var uniques = [];
+    for(value in frequency) {
+        uniques.push(value);
+    }
+
+    // sort the uniques array in descending order by frequency
+    function compareFrequency(a, b) {
+        return frequency[b] - frequency[a];
+    }
+
+    return uniques.sort(compareFrequency);
+}
+
+
+ 
+
 /*
 *This function uses an SQL query to get the desired information. 
 * We first query for any contacts that the person has an event that day, followed by 
@@ -29,7 +71,7 @@ var api = {
         request.azureMobile.data.execute(query)
         .then(function(results) {
             var user_id = results[0]["id"];
-            var query2 = {sql: 'SELECT DISTINCT c.name as name, ec.contact_id as cid FROM contacts c, event_history e, event_contacts ec where e.user_id = @user_id and e.id = ec.event_id and c.id = ec.contact_id  GROUP BY cid',
+            var query2 = {sql: 'SELECT DISTINCT c.name as name, ec.contact_id as cid FROM contacts c, event_history e, event_contacts ec where e.user_id = @user_id and e.id = ec.event_id and c.id = ec.contact_id',
             parameters: [
                 {     name: 'user_id', value: user_id }
             ]
@@ -38,8 +80,14 @@ var api = {
             .then(function(results) {
                 var tempArray = []
                 tempArray = results
+                var finalArray = []
+
+                for (var i = 0; i < tempArray.length; i++) {
+                    finalArray.push(tempArray[i]["name"])
+                }
+                finalArray = sortByFrequencyAndRemoveDuplicates(finalArray)
                 if (request.query.type == "frequent") {
-                    response.send(tempArray);
+                    response.send(finalArray);
                 }
             });
         });
