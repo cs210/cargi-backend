4b6fa91426ba8e5931b57c8dc65cde0c8b9c303d
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-05-30 05:32:25 -0700

    contacts

--- api/contacts.js
+++ api/contacts.js
@@ -21,7 +21,7 @@ function checkForDuplicates(results, name) {
 
 var api = {
     get: function(request, response, next) {
-        var query = {sql: "SELECT DISTINCT e.user_id as userid, c.name as name, ec.contact_id as cid, c.id as cid1 FROM contacts c, event_history e, event_contacts ec where (e.id LIKE '%' + ec.event_id + '%' or ec.event_id LIKE '%' + e.id + '%') and (c.id LIKE '%' + ec.contact_id + '%' or ec.contact_id LIKE '%' + c.id + '%')"
+        var query = {sql: 'SELECT DISTINCT e.user_id as userid, c.name as name, ec.contact_id as cid, c.id as cid1 FROM contacts c, event_history e, event_contacts ec where (e.id LIKE '%' + ec.event_id + '%' or ec.event_id LIKE '%' + e.id + '%') and (c.id LIKE '%' + ec.contact_id + '%' or ec.contact_id LIKE '%' + c.id + '%')'
             };
         request.azureMobile.data.execute(query)
         .then(function(results) {
