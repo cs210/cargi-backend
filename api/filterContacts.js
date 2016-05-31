d609c47e5e73e7e87f43d4367f076a823943bb5c
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-05-31 02:19:38 -0700

    filterContacts

--- api/filterContacts.js
+++ api/filterContacts.js
@@ -80,6 +80,7 @@ var api = {
 
                 for (var i = 0; i < results.length; i++) {
                     var id5String = concat("Optional(", results[i]["id5"], ")");
+                    response.send(id5String)
                     if (results[i]["id5"] != user_id && id5String != user_id) {
                         continue;
                     }
