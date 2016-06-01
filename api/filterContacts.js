79784c8eadbe8598bacdeaa9a50402fbf25aabc5
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-06-01 16:18:59 -0700

    api

--- api/filterContacts.js
+++ api/filterContacts.js
@@ -68,10 +68,10 @@ var api = {
         request.azureMobile.data.execute(query)
         .then(function(results) {
             if (results.length == 0) {
-            response.send(Email does not exist);
+            response.send("Email does not exist");
             }
             var user_id = results[0]["id"]
-            response.send(user_id)
+            response.send(user_id);
 
             var query2 = {sql: "SELECT c.name as name, c.id as id1, e.id as id2, ec.contact_id as id3, ec.event_id as id4, e.user_id as id5 FROM contacts c, event_history e, event_contacts ec where e.user_id = @user_id",
             parameters: [
