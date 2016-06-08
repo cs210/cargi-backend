d5fcf3e77a45ba10784af556084eaaed5bb02694
Kartik Sawhney <sawhney.kartik@gmail.com>
2016-06-08 01:50:43 -0700

    SQL query

--- api/filterContacts.js
+++ api/filterContacts.js
@@ -73,7 +73,7 @@ var api = {
 
             var user_id = results[0]["id"]
 
-            var query2 = {sql: 'SELECT c.name as name, c.id as id1, e.id as id2, ec.contact_id as id3, ec.event_id as id4 FROM contacts c, event_history e, event_contacts ec where id1 = id3 AND id2 = id4'
+            var query2 = {sql: 'SELECT c.name as name, c.id as id1, e.id as id2, ec.contact_id as id3, ec.event_id as id4 FROM contacts c, event_history e, event_contacts ec where c.id = ec.contact_id AND e.id = ec.event_id'
             };
             request.azureMobile.data.execute(query2)
             .then(function(results) {
